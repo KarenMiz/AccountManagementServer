c487f1f9fceba22a81fa587411d159d0583205b1
KarenMiz <155911291+KarenMiz@users.noreply.github.com>
2025-07-08 13:53:06 +0300

    Update ExpenseCategoriesScript.sql

--- Sql/ExpenseCategoriesScript.sql
+++ Sql/ExpenseCategoriesScript.sql
@@ -1,12 +1,4 @@
-IF OBJECT_ID('dbo.ExpenseCategories', 'U') IS NULL
-BEGIN
-    CREATE TABLE dbo.ExpenseCategories (
-        ExpenseCategoryId INT IDENTITY(1,1) PRIMARY KEY,
-        Name NVARCHAR(255) NOT NULL
-    );
-END
-
-INSERT INTO dbo.ExpenseCategories (Name) VALUES 
+INSERT INTO ExpenseCategories (Name) VALUES 
 ('Shopping'),
 ('Car'),
 ('Insurances'),
