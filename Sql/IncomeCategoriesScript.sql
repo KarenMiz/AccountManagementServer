a07fa6182b5335d0d864edb8a45bf75baaae1399
KarenMiz <155911291+KarenMiz@users.noreply.github.com>
2025-07-08 13:32:17 +0300

    Update IncomeCategoriesScript.sql

--- Sql/IncomeCategoriesScript.sql
+++ Sql/IncomeCategoriesScript.sql
@@ -1,4 +1,12 @@
-INSERT INTO IncomeCategories (Name) VALUES 
+IF OBJECT_ID('dbo.IncomeCategories', 'U') IS NULL
+BEGIN
+    CREATE TABLE dbo.IncomeCategories (
+        IncomeCategoryId INT IDENTITY(1,1) PRIMARY KEY,
+        Name NVARCHAR(255) NOT NULL
+    );
+END
+
+INSERT INTO dbo.IncomeCategories (Name) VALUES 
 ('FirstSalary'),
 ('SecondSalary'),
 ('Allowance'),
